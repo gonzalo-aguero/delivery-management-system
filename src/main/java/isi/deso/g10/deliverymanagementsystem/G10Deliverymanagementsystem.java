036de98722cf1e8248bbf0b23893406b53feaf0e
Gonzalo Aguero <gonzaloaguerodev@gmail.com>
2024-08-22 17:21:42 -0300

    First Commit 😎

---
+++ src/main/java/isi/deso/g10/deliverymanagementsystem/G10Deliverymanagementsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package isi.deso.g10.deliverymanagementsystem;
+
+/**
+ *
+ * @author gonzalo-aguero
+ */
+public class G10Deliverymanagementsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
