a0aa8fe4c9b37ffb8e58688df2ea446c6e26635f
Lucas De Lia <lucasdelia018@gmail.com>
2024-08-30 15:57:23 -0300

    agregue clases y metodos de la etapa dos me falta terminarlos

---
+++ src/main/java/isi/deso/g10/deliverymanagementsystem/Bebida.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isi.deso.g10.deliverymanagementsystem;
+
+/**
+ *
+ * @author lucas
+ */
+public class Bebida  extends ItemMenu {
+    private double graduacionAlcoholica;
+    //sustituye al atributo tamanio del diagrama de clases del tp
+    private double volumenEnMl;
+
+    
+}
