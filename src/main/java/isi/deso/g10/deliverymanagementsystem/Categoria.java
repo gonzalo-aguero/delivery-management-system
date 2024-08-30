dfd1c774a514846c9a51da1c5fc211d611a9b881
Lucas De Lia <lucasdelia018@gmail.com>
2024-08-30 15:57:32 -0300

    agregue clases y metodos de la etapa dos me falta terminarlos

---
+++ src/main/java/isi/deso/g10/deliverymanagementsystem/Categoria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isi.deso.g10.deliverymanagementsystem;
+
+/**
+ *
+ * @author lucas
+ */
+public class Categoria {
+    private int id;
+    private String descripcion;
+    private ItemMenu tipoItem;
+}
