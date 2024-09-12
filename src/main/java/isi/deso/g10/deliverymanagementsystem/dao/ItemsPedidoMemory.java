268301f9f99ceb52ecc739e2405bc0c200972c62
Rulopwd40 <132414402+Rulopwd40@users.noreply.github.com>
2024-09-12 17:43:08 -0300

    -Cambie los parametros de los metodos de ItemsPedidoDao y ItemsPedidoMemory; -Metodo buscarId implementado

---
+++ src/main/java/isi/deso/g10/deliverymanagementsystem/dao/ItemsPedidoMemory.java
@@ -0,0 +1,104 @@
+package isi.deso.g10.deliverymanagementsystem.dao;
+
+import isi.deso.g10.deliverymanagementsystem.classes.Bebida;
+import isi.deso.g10.deliverymanagementsystem.classes.Categoria;
+import isi.deso.g10.deliverymanagementsystem.classes.Comida;
+import isi.deso.g10.deliverymanagementsystem.classes.ItemMenu;
+import isi.deso.g10.deliverymanagementsystem.classes.Vendedor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import isi.deso.g10.deliverymanagementsystem.dao.*;
+
+public class ItemsPedidoMemory implements ItemsPedidoDao {
+
+    /*
+    private File itemsFile;
+    private File categoriasFile;
+    private List<Categoria> categorias;
+    private List<ItemMenu> items;
+     */
+ /*
+    public ItemsPedidoMemory(String itemsDir, String categoriasDir) throws IOException{
+        categoriasFile= new File(categoriasDir);
+        
+        try(BufferedReader br= new BufferedReader(new FileReader(categoriasFile))){
+              //Toma una linea 
+            String line;
+              while ((line = br.readLine()) != null) {
+                // Separar los valores de la línea por coma
+                String[] atributos = line.split(",");
+                // Procesar los valores
+                Categoria categoria = new Categoria(Integer.parseInt(atributos[0]),atributos[1],atributos[2]);
+                
+            }
+        }
+        catch(IOException e){
+            
+        }
+      
+        itemsFile= new File(itemsDir);
+        
+        try(BufferedReader br= new BufferedReader(new FileReader(itemsFile))){
+            //Toma una linea 
+            String line;
+              while ((line = br.readLine()) != null) {
+                // Separar los valores de la línea por coma
+                String[] values = line.split(",");
+                // Procesar los valores
+                for (String value : values) {
+                    
+                }
+            }
+        }
+        catch(IOException e) {
+             
+        }
+    }
+     */
+   
+    
+    @Override
+    public ItemMenu buscarId(int id, List<Vendedor> Vendedores) throws ItemNoEncontradoException {
+    return Vendedores.stream()
+            .flatMap(vendedor -> vendedor.getMenu().stream()) // Combina todos los menús de todos los vendedores
+            .filter(item -> item.getId() == id)
+            .findFirst() 
+            .orElseThrow(() -> new ItemNoEncontradoException("Item con id " + id + " no encontrado."));
+    }
+
+    @Override
+    public ItemMenu buscarNombre(String nombre, List<Vendedor> Vendedores) throws ItemNoEncontradoException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<ItemMenu> buscarPorRangoPrecio(double minimo, double maximo, List<Vendedor> Vendedores) throws ItemNoEncontradoException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<ItemMenu> buscarPorCategoria(Categoria categoria, List<Vendedor> Vendedores) throws ItemNoEncontradoException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Comida> buscarComidas(List<Vendedor> Vendedores) throws ItemNoEncontradoException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Bebida> buscarBebidas(List<Vendedor> Vendedores) throws ItemNoEncontradoException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<ItemMenu> buscarPorRestaurante(Vendedor vendedor) throws ItemNoEncontradoException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+
+   
+}
