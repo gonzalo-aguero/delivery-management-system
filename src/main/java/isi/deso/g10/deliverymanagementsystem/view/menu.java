52dc880b4d9bd065e970330ff9161ce531cd78d0
An7u4n <miguelitocentu@hotmail.com>
2024-10-24 12:24:08 -0300

    Avance interfaz de usuario

---
+++ src/main/java/isi/deso/g10/deliverymanagementsystem/view/menu.java
@@ -0,0 +1,12 @@
+package isi.deso.g10.deliverymanagementsystem.view;
+
+import javax.swing.*;
+
+public class menu {
+    private JPanel menuPanel;
+    private JButton vendedoresButton;
+    private JButton clientesButton;
+    private JButton itemsMenuButton;
+    private JButton pedidosButton;
+    private JTable table1;
+}
